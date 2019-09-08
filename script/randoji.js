fc61d2d2895e6ce94d827e3911c614e9383dd056
wmplayer <wmplayer@hotmail.com>
2019-09-08 16:37:38 -0700

    Some various updates

--- script/randoji.js
+++ script/randoji.js
@@ -39,6 +39,72 @@ function pairedRangesRandom(pairs)
     return pairs[subIndex] + (randomIndexIntoTotalCount - skippedTotal);
 }
 
+function testCaman()
+{
+    Caman("#canvas", function () {
+        // // Explicitly give the R, G, and B values of the
+        // // color to shift towards.
+        
+        // // Arguments: (R, G, B, strength)
+        this.resetOriginalPixelData();
+
+        this.newLayer(function () {
+            // Change the blending mode
+            this.setBlendingMode("multiply");
+        
+            // Change the opacity of this layer
+            this.opacity(30);
+        
+            // Now we can *either* fill this layer with a
+            // solid color...
+            this.fillColor('#159915');
+
+            // ... or we can copy the contents of the parent
+            // layer to this one.
+            this.copyParent();
+        
+            // Now, we can call any filter function though the
+            // filter object.
+            this.filter.brightness(10);
+            this.filter.contrast(20);
+            });
+
+        // this.render();
+
+        this.colorize(25, 180, 25, 10).render();
+    });
+}
+
+function convertCanvasToImage(canvas) {
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image;
+}
+
+function drawTest()
+{
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    //ctx.fillStyle = "#909090";
+    //ctx.fillRect(0, 0, c.width, c.height);
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.font = "normal 300px Segoe UI";
+    let rj = randoji();
+
+    elem=document.createElement('p')
+    elem.innerHTML = randoji();
+    ctx.fillText(elem.innerHTML, -16, 280);
+
+    // let cback = document.getElementById("back_buffer");
+    // let cbackctx = cback.getContext("2d");
+    // cbackctx.font = "normal 384px Segoe UI";
+    // cbackctx.fillText(elem.innerHTML, -90, 384);
+
+    // var img = convertCanvasToImage(cback);
+    // var jm = document.getElementById("joshymage");
+    // jm.image = img;
+}
+
 function randomizeAll(rowMax, colMax)
 {
     for (let row = 0; row < rowMax; row++)
@@ -50,6 +116,31 @@ function randomizeAll(rowMax, colMax)
         }
     }
 }
+
+function randomizeAndCreateDIVs(parentGrid, rowMax, colMax)
+{
+    let allInnerHTML = "";
+    let columns = "auto";
+    for (let row = 0; row < rowMax; row++)
+    {
+        for (let col = 0; col < colMax; col++)
+        {
+            allInnerHTML += '\r\n<div id="entry'  + row + col + '">' + randoji() + '</div>';
+            if (row == 0) if (col > 0) columns += " auto";
+        }
+    }
+    parentGrid.style.gridTemplateColumns = columns;
+    parentGrid.innerHTML = allInnerHTML;   
+}
+
+var g_AllMojiRanges =
+[
+    [["Smileys & Emotion"],
+        ["face-smiling", 0x1F600, 0x1F607],
+        ["face-affection", 0x1F970, 0x1F607],
+        ["face-smiling", 0x1F600, 0x1F607],
+        ["face-smiling", 0x1F600, 0x1F607]]
+];
 
 function quickList(rawString)
 {
